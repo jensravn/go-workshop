35d3081bf5469c85e3c9c29b97d491a11257b518
jensravn <jens.ravn.jensen@ingka.com>
2023-06-09 13:29:58 +0200

    put

--- README.md
+++ README.md
@@ -92,7 +92,39 @@ func main() {
 }
 ```
 
-### post
+### put
+
+```go
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			t := thing{
+				Message: "Hello world",
+			}
+			s, err := json.Marshal(&t)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			b := []byte(s)
+			w.Write(b)
+			return
+		}
+		if r.Method == http.MethodPut {
+			var t thing
+			err := json.NewDecoder(r.Body).Decode(&t)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			log.Printf("got thing: %#v", t)
+			return
+		}
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	})
+	http.ListenAndServe(":8080", nil)
+}
+```
 
 ### db file
 
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -11,15 +12,30 @@ type thing struct {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t := thing{
-			Message: "Hello world",
+		if r.Method == http.MethodGet {
+			t := thing{
+				Message: "Hello world",
+			}
+			s, err := json.Marshal(&t)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			b := []byte(s)
+			w.Write(b)
+			return
 		}
-		s, err := json.Marshal(&t)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		if r.Method == http.MethodPut {
+			var t thing
+			err := json.NewDecoder(r.Body).Decode(&t)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			log.Printf("got thing: %#v", t)
+			return
 		}
-		b := []byte(s)
-		w.Write(b)
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	})
 	http.ListenAndServe(":8080", nil)
 }
